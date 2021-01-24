0c07fe69c2e2169dfdbead887b246768a2d33496
NATE-DESKTOP\Nate <nari0015@hotmail.com>
2021-01-23 18:37:15 -0700

    First commit

---
+++ src/main/java/Main/Main.java
@@ -0,0 +1,30 @@
+
+package Main;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(modid = Reference.MODID, name = Reference.MODNAME, version = Reference.MODID, acceptedMinecraftVersions = Reference.ACCEPTED_MINECRAFT_VERSIONS)
+public class Main 
+{
+        @EventHandler
+	public void preInit(FMLPreInitializationEvent event) 
+        {
+            System.out.println(Reference.MODID + ":preInit");
+	}
+	
+	@EventHandler
+	public void init(FMLInitializationEvent event) 
+        {
+            System.out.println(Reference.MODID + ":init");
+	}
+	
+	@EventHandler
+	public void postInit(FMLPostInitializationEvent event) 
+        {
+            System.out.println(Reference.MODID + ":postInit");
+	}
+}
---
+++ src/main/java/Main/Reference.java
@@ -0,0 +1,10 @@
+
+package Main;
+
+public class Reference 
+{
+    public static final String MODID = "calamitybadges";
+    public static final String MODNAME = "Pixelmon Calamity Version Custom Badges";
+    public static final String VERSION = "0.0.1";
+    public static final String ACCEPTED_MINECRAFT_VERSIONS = "1.12";
+}
